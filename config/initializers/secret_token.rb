c87aa58fe01ea559300920fe74f1c2f999e1dcbd
Gary Lutes <garylutes@gmail.com>
2011-11-13 01:50:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sfbooks::Application.config.secret_token = '817da9e0555a12b97dc7ebc8dd4d93016ee1d127fc55b8d8a0bac83e127a74550413f6d29be724461239ba1b1a85d96881e5233ee0e2f39bb088676c2e18d593'
